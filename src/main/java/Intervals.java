73f554c0f1a636f12f32d4964a131a83b7e76f2d
normanridus <normanridus1990@gmail.com>
2023-03-13 15:00:39 +0300

    Add: maven, Interval class, make project structure

---
+++ src/main/java/Intervals.java
@@ -0,0 +1,10 @@
+public class Intervals {
+
+    public static String intervalConstruction(String[] args) {
+        return null;
+    }
+
+    public static String intervalIdentification(String[] args) {
+        return null;
+    }
+}
